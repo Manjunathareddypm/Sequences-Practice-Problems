f476d0ec833d39514f7fa39a41899f000748d2a7
Manjunatha Reddy PM <mreddypm@gmail.com>
2022-11-05 19:15:07 +0530

    sumandavgof5rannum

---
+++ 2digrannum.sh
@@ -0,0 +1,9 @@
+num1=$(( (RANDOM%99) + 100))
+num2=$(( (RANDOM%99) + 100))
+num3=$(( (RANDOM%99) + 100))
+num4=$(( (RANDOM%99) + 100))
+num5=$(( (RANDOM%99) + 100))
+sum=$(($num1+$num2+$num3+$num4+$num5))
+avg=$(($sum / 5))
+echo "sum of five numbers = $sum"
+echo  "avgerage of five numbers = $avg"
