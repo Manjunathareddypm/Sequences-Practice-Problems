4a9f83c425d53c1e06a939d45b179ce9bf0f3ffc
Manjunatha Reddy PM <mreddypm@gmail.com>
2022-11-12 16:25:54 +0530

    changed val 100 to 1

--- 2digrannum.sh
+++ 2digrannum.sh
@@ -1,8 +1,8 @@
-num1=$(( (RANDOM%99) + 100))
-num2=$(( (RANDOM%99) + 100))
-num3=$(( (RANDOM%99) + 100))
-num4=$(( (RANDOM%99) + 100))
-num5=$(( (RANDOM%99) + 100))
+num1=$(( (RANDOM%99) + 1))
+num2=$(( (RANDOM%99) + 1))
+num3=$(( (RANDOM%99) + 1))
+num4=$(( (RANDOM%99) + 1))
+num5=$(( (RANDOM%99) + 1))
 sum=$(($num1+$num2+$num3+$num4+$num5))
 avg=$(($sum / 5))
 echo "sum of five numbers = $sum"
